97da1cf2da751c453e6c8d530903568796d95457
MouraRei <reinaldomoura.contato@outlook.com>
2021-11-17 15:50:28 -0300

    Pontos flutuantes

---
+++ Pontos Flutuantes.cs
@@ -0,0 +1,15 @@
+namespace PontosFlutuantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vacinadas = 60.2;
+            Console.WriteLine("Total de pessoas no Brasil vacinadas contra o Covid é de " + vacinadas +"%.");
+
+            Console.WriteLine("Fim da execução. Tecle para sair.");
+
+            Console.ReadLine();
+        }
+    }
+}
